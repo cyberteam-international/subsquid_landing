234973bea2c76c2971213d3840099ede343673f0
Snegirev <sae.snegirev@gmail.com>
2023-09-04 13:11:45 +0500

    fix

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -70,34 +70,12 @@ export default function Footer() {
 
     return (
         <footer className={'footer'}>
-            {/*<div className="footer__blops-1">*/}
-            {/*    <svg width="2045" height="1251" viewBox="0 0 2045 1251" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-            {/*        <g filter="url(#filter0_f_502_2926)">*/}
-            {/*            <path d="M777.727 890.922C704.068 1000.54 458.857 934.024 345.459 887.067C216.784 845.004 385.221 724.857 562.853 736.939C740.485 749.02 891.035 427.813 981.259 340.409C1071.48 253.006 1251.95 342.208 1647.14 313.286C2042.33 284.365 1112.25 745.548 1015.52 560.074C918.78 374.6 869.801 753.904 777.727 890.922Z" fill="#FBC688"/>*/}
-            {/*        </g>*/}
-            {/*        <defs>*/}
-            {/*            <filter id="filter0_f_502_2926" x="0.150391" y="0.901611" width="2044.28" height="1249.54" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">*/}
-            {/*                <feFlood floodOpacity="0" result="BackgroundImageFix"/>*/}
-            {/*                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>*/}
-            {/*                <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_502_2926"/>*/}
-            {/*            </filter>*/}
-            {/*        </defs>*/}
-            {/*    </svg>*/}
-            {/*</div>*/}
-            {/*<div className="footer__blops-2">*/}
-            {/*    <svg width="1979" height="839" viewBox="0 0 1979 839" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-            {/*        <g filter="url(#filter0_f_517_3115)">*/}
-            {/*            <path d="M1375.15 137.858C1499.47 38.71 1733.43 237.806 1834.88 349.748C1954.66 463.009 1724.8 529.654 1540.26 429.706C1355.72 329.758 1061.11 651.591 928.37 715.557C795.631 779.524 640.23 583.626 206.403 429.706C-227.425 275.786 957.507 155.849 983.408 429.706C1009.31 703.563 1219.75 261.794 1375.15 137.858Z" fill="#EFF6FF"/>*/}
-            {/*        </g>*/}
-            {/*        <defs>*/}
-            {/*            <filter id="filter0_f_517_3115" x="0" y="0" width="1979" height="839" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">*/}
-            {/*                <feFlood floodOpacity="0" result="BackgroundImageFix"/>*/}
-            {/*                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>*/}
-            {/*                <feGaussianBlur stdDeviation="55.5" result="effect1_foregroundBlur_517_3115"/>*/}
-            {/*            </filter>*/}
-            {/*        </defs>*/}
-            {/*    </svg>*/}
-            {/*</div>*/}
+            <div className="footer__blops-1">
+                <img src="/blub-5.png" alt=""/>
+            </div>
+            <div className="footer__blops-2">
+                <img src="/blub-6.png" alt=""/>
+            </div>
 
             <div className="container">
                 <div className="footer__wrapper">
