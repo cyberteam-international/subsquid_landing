409dddf26404d86325fffd689cf329f82758ee83
Dyv1k <duvanov.ilja@gmail.com>
2023-09-24 17:44:30 +0300

    add hint for addProcessors

--- src/components/ApiCosts/AddProcessor.tsx
+++ src/components/ApiCosts/AddProcessor.tsx
@@ -1,5 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useWindowWidth } from "@react-hook/window-size";
+
+import useMousePosition from "@/hooks/useMousePosition";
 
 import { NewProcessorsContext } from "@/app/pricing/context";
 
@@ -16,6 +18,10 @@ export default function AddProcessor({ tabsProfile }: Props) {
     const [newProcessors, setNewProcessors] = useContext(NewProcessorsContext)
 
     const windowWidth = useWindowWidth()
+
+    const blockRef = useRef<HTMLDivElement>(null)
+
+    const cursorPosition = useMousePosition(blockRef)
 
     const renderItem: IApiCostsRadio = {
         title: `Processor profile ${newProcessors.state.length + 2}`,
@@ -72,9 +78,12 @@ export default function AddProcessor({ tabsProfile }: Props) {
         }
     }
 
+    useEffect(()=>{console.log(cursorPosition)}, [cursorPosition])
+
     return (
         <>
             <div
+                ref={blockRef}
                 onClick={pushProcessor}
                 className={
                     newProcessors.render.length < 10 && tabsProfile !== 'COLLOCATED'?
@@ -82,20 +91,24 @@ export default function AddProcessor({ tabsProfile }: Props) {
                     :`${style["add"]} ${style["add_disable"]}`
                 }
             >
-                <p>Add processor</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path d="M8 3.33203V12.6654" stroke="#3880EC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M3.33203 8H12.6654" stroke="#3880EC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
+                <div className={style["add__wrapper"]}>
+                    <p>Add processor</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                        <path d="M8 3.33203V12.6654" stroke="#3880EC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M3.33203 8H12.6654" stroke="#3880EC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </div>
+                {(tabsProfile === 'COLLOCATED' && cursorPosition.isInside && windowWidth >= 768) && (
+                    <p 
+                        className={style["add__hint"]}
+                        style={{top: `${cursorPosition.y + 25}px`, left: `${cursorPosition.x - 40}px`}}
+                    >
+                        Not available for collocated profiles
+                    </p>
+                )}
             </div>
             {(newProcessors.render.length > 0 && newProcessors.render.length % 2 !== 0 && windowWidth >= 768) && (
-                <div style={{opacity: 0, pointerEvents: 'none'}}>
-                    {/* <ApiCostsFieldProcessor
-                        field={renderItem}
-                        selectValuesState={[newProcessors, setNewProcessors]}
-                        tabsProfile={tabsProfile}
-                    /> */}
-                </div>
+                <div style={{opacity: 0, pointerEvents: 'none'}}></div>
             )}
         </>
     )
