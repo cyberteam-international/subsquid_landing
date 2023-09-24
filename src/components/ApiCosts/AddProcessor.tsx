fd1309e819364ae3d5f10d101a396af5663dbe5e
Dyv1k <duvanov.ilja@gmail.com>
2023-09-24 18:21:42 +0300

    set copy animation

--- src/components/ApiCosts/AddProcessor.tsx
+++ src/components/ApiCosts/AddProcessor.tsx
@@ -78,8 +78,6 @@ export default function AddProcessor({ tabsProfile }: Props) {
         }
     }
 
-    useEffect(()=>{console.log(cursorPosition)}, [cursorPosition])
-
     return (
         <>
             <div
